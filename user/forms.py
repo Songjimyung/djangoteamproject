8a3182d1c180f6d12087213fd0801b1f36d5a13d
yunsu68 <126667304+yunsu68@users.noreply.github.com>
2023-04-13 04:23:05 +0900

    Delete user directory

--- user/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from user.models import UserModel
-
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = UserModel
-        fields = ['username', 'password', 'email', 'bio']
