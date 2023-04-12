34b739cb603839e72d7f28efdc0eb5028e4ac37a
Songjimyung <116045723+Songjimyung@users.noreply.github.com>
2023-04-12 21:25:16 +0900

    Add files via upload

--- boards/forms.py
+++ boards/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 from .models import Board
-from django.contrib.auth.models import User
+
+# forms.py
 
 
-# 게시글 작성 폼
-# 가져오는 값 : id, title, content
 class BoardForm(forms.ModelForm):
     class Meta:
         model = Board
-        fields = ('title', 'content', 'author')
+        fields = ['board_id', 'title', 'content']
--- boards/models.py
+++ boards/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.conf import settings
+
+# Create your models here.
 
 
-# 게시글 작성 모델
 class Board(models.Model):
-    title = models.CharField(max_length=255)
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    board_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
     content = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.title
+    class Meta:
+        db_table = 'board'
